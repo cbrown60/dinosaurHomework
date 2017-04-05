fa6d02b4a8384164e8a8f9c736c78b67f23bd5af
Chris <cbrown605@googlemail.com>
2017-04-05 08:36:05 +0100

    test for over 2 not working yet

--- park.js
+++ park.js
@@ -13,8 +13,33 @@ Park.prototype = {
       var position = this.enclosure.indexOf(dinosaur)
       this.enclosure.splice(position, 1)
     }
-  }
+  },
 
+  // moreThanTwo : function(park){
+  //   var totalOver2 = 0
+  //   for(var dinosaur of park.enclosure){
+  //     if(dinosaur.offspringPerYear > 2){
+  //       return totalOver2 += 1
+  //     }
+
+  //   }
+  // }
+
+moreThanTwo :function(){
+  var count = 0;
+  for(var i = 0; i < park.length; ++i){
+      if(park[i] > 2)
+          count++;
+  }
+  
+}
 }
 
 module.exports = Park
+
+// calculateGross : function(shoppingBasket){
+//   var total =0
+//   for(var item of shoppingBasket.items){
+//     total += item.price
+//   }
+//   return total 
--- specs/park_specs.js
+++ specs/park_specs.js
@@ -7,7 +7,7 @@ describe('Park', function () {
   var park
 
   beforeEach(function(){
-    dinosaur = new Dinosaur('T-Rex', 2)
+    dinosaur = new Dinosaur('T-Rex', 3)
     park = new Park()
   })
 
@@ -29,8 +29,24 @@ describe('Park', function () {
   it('can remove dinosaur by type', function(){
   park.addDinosaur(dinosaur)
   var dinosaur2 = new Dinosaur("diplodocus", 6)
+  park.addDinosaur(dinosaur2)
   park.removeDinosaur(dinosaur2)
   assert.strictEqual(1, park.enclosure.length)
+  })
+
+  it('return dinosaurs with average babie over 2', function(){
+
+    park.addDinosaur(dinosaur)
+    var dinosaur3 = new Dinosaur("big dino", 6)
+    park.addDinosaur(dinosaur3)
+    var dinosaur4 = new Dinosaur("small dino", 1)
+    park.addDinosaur(dinosaur4)
+    var dinosaur5 = new Dinosaur("little dino", 1)
+    park.addDinosaur(dinosaur5)
+    var dinosaur6 = new Dinosaur("medium dino", 4)
+    park.addDinosaur(dinosaur6)
+    assert.strictEqual(3, park.moreThanTwo(park))
+
   })
  
 })
